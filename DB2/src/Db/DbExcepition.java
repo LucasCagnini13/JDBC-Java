24ebbea7823347ee6a64babced4aa73591349670
Lucas <lucasplaying66571289@gmail.com>
2023-12-23 14:21:46 -0300

    Project created

---
+++ DB2/src/Db/DbExcepition.java
@@ -0,0 +1,10 @@
+package Db;
+
+public class DbExcepition extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbExcepition(String msg) {
+		super(msg);
+	}
+}
